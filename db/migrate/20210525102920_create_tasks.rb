b270e5060c5f2aef986d5a5510b52b76218caa9d
Tim White <tim94white@gmail.com>
2021-05-25 16:08:59 +0100

    rails task manager

---
+++ db/migrate/20210525102920_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.string :details
+      t.string :text
+      t.boolean :completed
+
+      t.timestamps
+    end
+  end
+end
